e9c550f4d76a4be6e2633421a89478db71abcb4f
npt306 <nptai212@clc.fitus.edu.vn>
2025-01-26 21:47:17 +0700

    business tag

--- src/pages/ProfileShop/GeneralInformation.tsx
+++ src/pages/ProfileShop/GeneralInformation.tsx
@@ -4,11 +4,12 @@ const GeneralInformation = () => {
     <div className="p-6 bg-white">
       {/* Header */}
       <div className="flex justify-between items-center pb-4 mb-4">
-        <h2 className="text-sm text-black">Thông tin cơ bản</h2>
+        <h2 className="text-xl text-black">Thông tin cơ bản</h2>
         <div className="space-x-4">
           {/* <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
             Xem Shop của tôi
           </button> */}
+          <Button label="Xem shop của tôi" />
           <Button label="Chỉnh sửa" />
         </div>
       </div>
@@ -17,7 +18,7 @@ const GeneralInformation = () => {
       <form className="space-y-6">
         {/* Tên Shop */}
         <div className="flex items-center space-x-4">
-          <label htmlFor="shopName" className="w-1/6 text-black text-sm">
+          <label htmlFor="shopName" className="w-1/6 text-right flex-shrink-0 text-black text-sm">
             Tên Shop
           </label>
           <div className="flex-grow">
@@ -30,7 +31,7 @@ const GeneralInformation = () => {
 
         {/* Logo của Shop */}
         <div className="flex items-center space-x-4">
-          <label className="w-1/6 text-black text-sm">Logo của Shop</label>
+          <label className="w-1/6 text-right flex-shrink-0 text-black text-sm">Logo của Shop</label>
           <div className="flex items-center space-x-4">
             <div
               className="w-24 h-24 bg-cover bg-center rounded-full"
@@ -50,12 +51,12 @@ const GeneralInformation = () => {
 
         {/* Mô tả Shop */}
         <div className="flex items-center space-x-4">
-          <label htmlFor="shopDesc" className="w-1/6 text-black text-sm">
+          <label htmlFor="shopDesc" className="w-1/6 text-right flex-shrink-0 text-black text-sm">
             Mô tả Shop
           </label>
           <div className="flex-grow">
             <span className="py-2 text-black text-sm ">
-              bc_ml2zyn5
+            Shop của chúng tôi là nơi cung cấp đa dạng các sản phẩm chất lượng cao, đáp ứng nhu cầu mua sắm của mọi khách hàng. Với không gian trưng bày hiện đại và thân thiện, chúng tôi cam kết mang lại trải nghiệm mua sắm thoải mái và tiện lợi. Các sản phẩm tại shop bao gồm thời trang, phụ kiện, đồ gia dụng, và nhiều mặt hàng khác được chọn lọc kỹ lưỡng từ các thương hiệu uy tín. Đội ngũ nhân viên nhiệt tình, chuyên nghiệp luôn sẵn sàng hỗ trợ, tư vấn để bạn tìm được sản phẩm phù hợp nhất. Hãy đến với chúng tôi để trải nghiệm sự khác biệt!
             </span>
           </div>
         </div>
